820ac823b99ead354e3fb7b983bde065b0a0c97b
rozalijr <williamz1official@gmail.com>
2024-09-03 15:41:51 +0300

    signup and login page

--- Lawproj/Lawapp/urls.py
+++ Lawproj/Lawapp/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 from .forms import UserLoginForm
 urlpatterns=[
-    path("", views.index, name='main'),
+    # path("", views.index, name='main'),
     path("sign-up/", views.SignUp.as_view(), name='signup'),
     path("logout/", views.logout_view, name="logout"),
     path("login/", views.LoginView.as_view(), name="login"),
