168a7f909ef6828acb929895f6c0895c6003baea
rozalijr <williamz1official@gmail.com>
2024-09-05 11:06:01 +0300

    login

--- Lawproj/Lawapp/urls.py
+++ Lawproj/Lawapp/urls.py
@@ -7,5 +7,5 @@ urlpatterns=[
     # path("", views.index, name='main'),
     path("signup/", views.SignUp.as_view(), name='signup'),
     path("logout/", views.logout_view, name="logout"),
-    path("login/", views.LoginView.as_view(), name="login"),
+    path("", views.LoginView.as_view(), name="login"),
 ]
